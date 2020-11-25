e638753e76add952c79ad28b4fbb9462851032f7
CozyDe <74243625+CozyDe@users.noreply.github.com>
2020-11-25 16:19:01 +0100

    Add files via upload

--- style.js
+++ style.js
@@ -1,6 +1,20 @@
-var navDown = document.querySelector("#toggledown");
-var bring = document.querySelector("#bring");
-navDown.addEventListener("click", function changeNav(){
-    bring.classList.toggle("is-active");
-    navDown.classList.toggle("change");
+var navTogg = document.querySelector("#burger-menu-icon");
+var result = document.querySelector("#menu");
+var searchMe = document.getElementById("search-me");
+var search = document.getElementById("search");
+var rmv = document.querySelector('.navCov');
+
+rmv.addEventListener("click", function(){
+  search.classList.remove('move');
 });
+
+  searchMe.addEventListener("click", function(){
+     search.style.cssText = "color: white;";
+     search.classList.toggle("move");
+  }); 
+
+
+navTogg.addEventListener("click", function toggNav() {
+      result.classList.toggle("slide");
+      navTogg.classList.toggle("times"); 
+  });
